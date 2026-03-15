743f8118f061402ba1379690c5b7c3fdbf9a2028
jaoneill03 <jaoneill03@gmail.com>
2026-03-14 19:26:35 -0500

    Update DisjointSet.cpp

--- DisjointSet.cpp
+++ DisjointSet.cpp
@@ -1,5 +1,5 @@
 // DisjointSet implementation using union by size and path compression
-// By Mary Elaine Califf and Joshua O'Neill
+// By Joshua O'Neill
 
 #include "DisjointSet.h"
 #include <iostream>
@@ -14,7 +14,6 @@ DisjointSet::DisjointSet(int numObjects)
 //recursive method to find the item -- does path compression on the way out of the recursion
 int DisjointSet::find(int objectIndex)
 {  
-    // to do -- see assignment instructions for details
     if(theArray[objectIndex] < 0){
         return objectIndex;
     }else{
@@ -24,7 +23,6 @@ int DisjointSet::find(int objectIndex)
 
 bool DisjointSet::doUnion(int objIndex1, int objIndex2)
 {
-    // to do -- see assignment instructions for details
     int root1 = find(objIndex1);
     int root2 = find(objIndex2);
     int temp;
